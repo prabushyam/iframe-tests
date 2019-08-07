0169ec03ef54b0628e91c1fe2a2c51dab5b04ea8
Michael Cieplak <michael.cieplak@gmail.com>
2019-08-07 14:39:34 -0700

    Initial test working with api server create crd / cr

--- apiserver_test.go
+++ apiserver_test.go
@@ -17,305 +17,217 @@ limitations under the License.
 package main
 
 import (
-	"bytes"
-	"context"
-	"fmt"
-	"io/ioutil"
-	"net/http"
-	"net/http/httptest"
-	"reflect"
+	"log"
 	"testing"
+	"time"
 
-	"k8s.io/api/core/v1"
-	"k8s.io/api/extensions/v1beta1"
-	"k8s.io/apimachinery/pkg/api/meta"
+	"github.com/ardielle/ardielle-go/rdl"
+	"github.com/yahoo/athenz/clients/go/zms"
+	athenz_domain "github.com/yahoo/k8s-athenz-syncer/pkg/apis/athenz/v1"
+	athenzClientset "github.com/yahoo/k8s-athenz-syncer/pkg/client/clientset/versioned"
+	"k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
+	apiextensionsclient "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	clientset "k8s.io/client-go/kubernetes"
-	restclient "k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/pager"
-	"k8s.io/klog"
-	"k8s.io/kubernetes/pkg/api/testapi"
-	api "k8s.io/kubernetes/pkg/apis/core"
-	"k8s.io/kubernetes/pkg/master"
+	genericapiserver "k8s.io/apiserver/pkg/server"
+	"k8s.io/client-go/rest"
+	"k8s.io/kubernetes/cmd/kube-apiserver/app"
+	"k8s.io/kubernetes/cmd/kube-apiserver/app/options"
 	"k8s.io/kubernetes/test/integration/framework"
+	"net"
 )
 
-func setup(t *testing.T, groupVersions ...schema.GroupVersion) (*httptest.Server, clientset.Interface, framework.CloseFunc) {
-	masterConfig := framework.NewIntegrationTestMasterConfig()
-	if len(groupVersions) > 0 {
-		resourceConfig := master.DefaultAPIResourceConfigSource()
-		resourceConfig.EnableVersions(groupVersions...)
-		masterConfig.ExtraConfig.APIResourceConfigSource = resourceConfig
-	}
-	_, s, closeFn := framework.RunAMaster(masterConfig)
+func TestFoo(t *testing.T) {
+	log.Println("inside foo")
 
-	clientSet, err := clientset.NewForConfig(&restclient.Config{Host: s.URL})
+	s := options.NewServerRunOptions()
+
+	listener, err := net.Listen("tcp4", "127.0.0.1:9999")
 	if err != nil {
-		t.Fatalf("Error in create clientset: %v", err)
+		log.Panic(err)
 	}
-	return s, clientSet, closeFn
+
+	s.InsecureServing.BindAddress = net.ParseIP("127.0.0.1")
+	s.InsecureServing.Listener = listener
+	s.Etcd.StorageConfig.Transport.ServerList = []string{framework.GetEtcdURL()}
+	s.SecureServing.ServerCert.CertDirectory = "/tmp"
+
+	completedOptions, err := app.Complete(s)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	if errs := completedOptions.Validate(); len(errs) != 0 {
+		log.Panicln(errs)
+	}
+
+	go app.Run(completedOptions, genericapiserver.SetupSignalHandler())
+	//if err != nil {
+	//	log.Panic(err)
+	//}
+	createCrd()
+	time.Sleep(time.Second * 15)
+	createCr()
+
+	time.Sleep(time.Minute * 10)
 }
 
-func verifyStatusCode(t *testing.T, verb, URL, body string, expectedStatusCode int) {
-	// We dont use the typed Go client to send this request to be able to verify the response status code.
-	bodyBytes := bytes.NewReader([]byte(body))
-	req, err := http.NewRequest(verb, URL, bodyBytes)
-	if err != nil {
-		t.Fatalf("unexpected error: %v in sending req with verb: %s, URL: %s and body: %s", err, verb, URL, body)
-	}
-	transport := http.DefaultTransport
-	klog.Infof("Sending request: %v", req)
-	resp, err := transport.RoundTrip(req)
-	if err != nil {
-		t.Fatalf("unexpected error: %v in req: %v", err, req)
-	}
-	defer resp.Body.Close()
-	b, _ := ioutil.ReadAll(resp.Body)
-	if resp.StatusCode != expectedStatusCode {
-		t.Errorf("Expected status %v, but got %v", expectedStatusCode, resp.StatusCode)
-		t.Errorf("Body: %v", string(b))
-	}
-}
+func createCrd() {
+	restConfig := &rest.Config{}
+	restConfig.Host = "http://127.0.0.1:9999"
 
-func path(resource, namespace, name string) string {
-	return testapi.Extensions.ResourcePath(resource, namespace, name)
-}
-
-func newRS(namespace string) *v1beta1.ReplicaSet {
-	return &v1beta1.ReplicaSet{
+	crd := &v1beta1.CustomResourceDefinition{
 		TypeMeta: metav1.TypeMeta{
-			Kind:       "ReplicaSet",
-			APIVersion: "extensions/v1beta1",
+			Kind:       "CustomResourceDefinition",
+			APIVersion: "apiextensions.k8s.io/v1beta1",
 		},
 		ObjectMeta: metav1.ObjectMeta{
-			Namespace:    namespace,
-			GenerateName: "apiserver-test",
+			Name: "athenzdomains.athenz.io",
 		},
-		Spec: v1beta1.ReplicaSetSpec{
-			Template: v1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{
-					Labels: map[string]string{"name": "test"},
+		Spec: v1beta1.CustomResourceDefinitionSpec{
+			Group: "athenz.io",
+			Scope: v1beta1.ClusterScoped,
+			Versions: []v1beta1.CustomResourceDefinitionVersion{
+				{
+					Name:    "v1",
+					Served:  true,
+					Storage: true,
 				},
-				Spec: v1.PodSpec{
-					Containers: []v1.Container{
-						{
-							Name:  "fake-name",
-							Image: "fakeimage",
-						},
-					},
-				},
+			},
+			Names: v1beta1.CustomResourceDefinitionNames{
+				Plural:     "athenzdomains",
+				Singular:   "athenzdomain",
+				Kind:       "AthenzDomain",
+				ShortNames: []string{"domain"},
+				ListKind:   "AthenzDomainList",
 			},
 		},
 	}
+
+	rClientset, err := apiextensionsclient.NewForConfig(restConfig)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	created, err := rClientset.ApiextensionsV1beta1().CustomResourceDefinitions().Create(crd)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Println("created:", created)
+
+	got, err := rClientset.ApiextensionsV1beta1().CustomResourceDefinitions().Get("athenzdomains.athenz.io", metav1.GetOptions{})
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Println("got:", got)
 }
 
-var cascDel = `
-{
-  "kind": "DeleteOptions",
-  "apiVersion": "` + testapi.Groups[api.GroupName].GroupVersion().String() + `",
-  "orphanDependents": false
-}
-`
+func createCr() {
+	restConfig := &rest.Config{}
+	restConfig.Host = "http://127.0.0.1:9999"
 
-// Tests that the apiserver returns 202 status code as expected.
-func Test202StatusCode(t *testing.T) {
-	s, clientSet, closeFn := setup(t)
-	defer closeFn()
-
-	ns := framework.CreateTestingNamespace("status-code", s, t)
-	defer framework.DeleteTestingNamespace(ns, s, t)
-
-	rsClient := clientSet.ExtensionsV1beta1().ReplicaSets(ns.Name)
-
-	// 1. Create the resource without any finalizer and then delete it without setting DeleteOptions.
-	// Verify that server returns 200 in this case.
-	rs, err := rsClient.Create(newRS(ns.Name))
-	if err != nil {
-		t.Fatalf("Failed to create rs: %v", err)
-	}
-	verifyStatusCode(t, "DELETE", s.URL+path("replicasets", ns.Name, rs.Name), "", 200)
-
-	// 2. Create the resource with a finalizer so that the resource is not immediately deleted and then delete it without setting DeleteOptions.
-	// Verify that the apiserver still returns 200 since DeleteOptions.OrphanDependents is not set.
-	rs = newRS(ns.Name)
-	rs.ObjectMeta.Finalizers = []string{"kube.io/dummy-finalizer"}
-	rs, err = rsClient.Create(rs)
-	if err != nil {
-		t.Fatalf("Failed to create rs: %v", err)
-	}
-	verifyStatusCode(t, "DELETE", s.URL+path("replicasets", ns.Name, rs.Name), "", 200)
-
-	// 3. Create the resource and then delete it with DeleteOptions.OrphanDependents=false.
-	// Verify that the server still returns 200 since the resource is immediately deleted.
-	rs = newRS(ns.Name)
-	rs, err = rsClient.Create(rs)
-	if err != nil {
-		t.Fatalf("Failed to create rs: %v", err)
-	}
-	verifyStatusCode(t, "DELETE", s.URL+path("replicasets", ns.Name, rs.Name), cascDel, 200)
-
-	// 4. Create the resource with a finalizer so that the resource is not immediately deleted and then delete it with DeleteOptions.OrphanDependents=false.
-	// Verify that the server returns 202 in this case.
-	rs = newRS(ns.Name)
-	rs.ObjectMeta.Finalizers = []string{"kube.io/dummy-finalizer"}
-	rs, err = rsClient.Create(rs)
-	if err != nil {
-		t.Fatalf("Failed to create rs: %v", err)
-	}
-	verifyStatusCode(t, "DELETE", s.URL+path("replicasets", ns.Name, rs.Name), cascDel, 202)
-}
-
-func TestAPIListChunking(t *testing.T) {
-	//if err := utilfeature.DefaultFeatureGate.Set(string(genericfeatures.APIListChunking) + "=true"); err != nil {
-	//	t.Fatal(err)
-	//}
-	s, clientSet, closeFn := setup(t)
-	defer closeFn()
-
-	ns := framework.CreateTestingNamespace("list-paging", s, t)
-	defer framework.DeleteTestingNamespace(ns, s, t)
-
-	rsClient := clientSet.ExtensionsV1beta1().ReplicaSets(ns.Name)
-
-	for i := 0; i < 4; i++ {
-		rs := newRS(ns.Name)
-		rs.Name = fmt.Sprintf("test-%d", i)
-		if _, err := rsClient.Create(rs); err != nil {
-			t.Fatal(err)
-		}
-	}
-
-	calls := 0
-	firstRV := ""
-	p := &pager.ListPager{
-		PageSize: 1,
-		PageFn: pager.SimplePageFunc(func(opts metav1.ListOptions) (runtime.Object, error) {
-			calls++
-			list, err := rsClient.List(opts)
-			if err != nil {
-				return nil, err
-			}
-			if calls == 1 {
-				firstRV = list.ResourceVersion
-			}
-			if calls == 2 {
-				rs := newRS(ns.Name)
-				rs.Name = "test-5"
-				if _, err := rsClient.Create(rs); err != nil {
-					t.Fatal(err)
-				}
-			}
-			return list, err
-		}),
-	}
-	listObj, err := p.List(context.Background(), metav1.ListOptions{})
-	if err != nil {
-		t.Fatal(err)
-	}
-	if calls != 4 {
-		t.Errorf("unexpected list invocations: %d", calls)
-	}
-	list := listObj.(metav1.ListInterface)
-	if len(list.GetContinue()) != 0 {
-		t.Errorf("unexpected continue: %s", list.GetContinue())
-	}
-	if list.GetResourceVersion() != firstRV {
-		t.Errorf("unexpected resource version: %s instead of %s", list.GetResourceVersion(), firstRV)
-	}
-	var names []string
-	if err := meta.EachListItem(listObj, func(obj runtime.Object) error {
-		rs := obj.(*v1beta1.ReplicaSet)
-		names = append(names, rs.Name)
-		return nil
-	}); err != nil {
-		t.Fatal(err)
-	}
-	if !reflect.DeepEqual(names, []string{"test-0", "test-1", "test-2", "test-3"}) {
-		t.Errorf("unexpected items: %#v", list)
-	}
-}
-
-func makeSecret(name string) *v1.Secret {
-	return &v1.Secret{
+	domain := "home.foo"
+	fakeDomain := getFakeDomain()
+	newCR := &athenz_domain.AthenzDomain{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "AthenzDomain",
+			APIVersion: "athenz.io/v1",
+		},
 		ObjectMeta: metav1.ObjectMeta{
-			Name: name,
+			Name: domain,
 		},
-		Data: map[string][]byte{
-			"key": []byte("value"),
+		Spec: athenz_domain.AthenzDomainSpec{
+			SignedDomain: fakeDomain,
+		},
+		Status: athenz_domain.AthenzDomainStatus{
+			Message: "",
 		},
 	}
+
+	versiondClient, err := athenzClientset.NewForConfig(restConfig)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	list, err := versiondClient.AthenzV1().AthenzDomains().List(metav1.ListOptions{})
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Println("list:", list)
+
+	created, err := versiondClient.AthenzV1().AthenzDomains().Create(newCR)
+	if err != nil {
+		log.Println("error creating athenz domain:", err)
+		return
+	}
+	log.Println("created cr:", created)
+
+	got, err := versiondClient.AthenzV1().AthenzDomains().Get(domain, metav1.GetOptions{})
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Println("got cr:", got)
 }
 
-func TestNameInFieldSelector(t *testing.T) {
-	s, clientSet, closeFn := setup(t)
-	defer closeFn()
-
-	numNamespaces := 3
-	namespaces := make([]*v1.Namespace, 0, numNamespaces)
-	for i := 0; i < 3; i++ {
-		ns := framework.CreateTestingNamespace(fmt.Sprintf("ns%d", i), s, t)
-		defer framework.DeleteTestingNamespace(ns, s, t)
-		namespaces = append(namespaces, ns)
-
-		_, err := clientSet.CoreV1().Secrets(ns.Name).Create(makeSecret("foo"))
-		if err != nil {
-			t.Errorf("Couldn't create secret: %v", err)
-		}
-		_, err = clientSet.CoreV1().Secrets(ns.Name).Create(makeSecret("bar"))
-		if err != nil {
-			t.Errorf("Couldn't create secret: %v", err)
-		}
+func getFakeDomain() zms.SignedDomain {
+	allow := zms.ALLOW
+	timestamp, err := rdl.TimestampParse("2019-06-21T19:28:09.305Z")
+	if err != nil {
+		panic(err)
 	}
 
-	testcases := []struct {
-		namespace       string
-		selector        string
-		expectedSecrets int
-	}{
-		{
-			namespace:       "",
-			selector:        "metadata.name=foo",
-			expectedSecrets: numNamespaces,
+	domainName := "home.foo"
+	username := "user.foo"
+	return zms.SignedDomain{
+		Domain: &zms.DomainData{
+			Modified: timestamp,
+			Name:     zms.DomainName(domainName),
+			Policies: &zms.SignedPolicies{
+				Contents: &zms.DomainPolicies{
+					Domain: zms.DomainName(domainName),
+					Policies: []*zms.Policy{
+						{
+							Assertions: []*zms.Assertion{
+								{
+									Role:     domainName + ":role.admin",
+									Resource: domainName + ".test:*",
+									Action:   "*",
+									Effect:   &allow,
+								},
+							},
+							Modified: &timestamp,
+							Name:     zms.ResourceName(domainName + ":policy.admin"),
+						},
+					},
+				},
+				KeyId:     "col-env-1.1",
+				Signature: "signature-policy",
+			},
+			Roles: []*zms.Role{
+				{
+					Members:  []zms.MemberName{zms.MemberName(username)},
+					Modified: &timestamp,
+					Name:     zms.ResourceName(domainName + ":role.admin"),
+					RoleMembers: []*zms.RoleMember{
+						{
+							MemberName: zms.MemberName(username),
+						},
+					},
+				},
+				{
+					Trust:    "parent.domain",
+					Modified: &timestamp,
+					Name:     zms.ResourceName(domainName + ":role.trust"),
+				},
+			},
+			Services: []*zms.ServiceIdentity{},
+			Entities: []*zms.Entity{},
 		},
-		{
-			namespace:       "",
-			selector:        "metadata.name=foo,metadata.name=bar",
-			expectedSecrets: 0,
-		},
-		{
-			namespace:       "",
-			selector:        "metadata.name=foo,metadata.namespace=ns1",
-			expectedSecrets: 1,
-		},
-		{
-			namespace:       "ns1",
-			selector:        "metadata.name=foo,metadata.namespace=ns1",
-			expectedSecrets: 1,
-		},
-		{
-			namespace:       "ns1",
-			selector:        "metadata.name=foo,metadata.namespace=ns2",
-			expectedSecrets: 0,
-		},
-		{
-			namespace:       "ns1",
-			selector:        "metadata.name=foo,metadata.namespace=",
-			expectedSecrets: 0,
-		},
-	}
-
-	for _, tc := range testcases {
-		opts := metav1.ListOptions{
-			FieldSelector: tc.selector,
-		}
-		secrets, err := clientSet.CoreV1().Secrets(tc.namespace).List(opts)
-		if err != nil {
-			t.Errorf("%s: Unexpected error: %v", tc.selector, err)
-		}
-		if len(secrets.Items) != tc.expectedSecrets {
-			t.Errorf("%s: Unexpected number of secrets: %d, expected: %d", tc.selector, len(secrets.Items), tc.expectedSecrets)
-		}
+		KeyId:     "colo-env-1.1",
+		Signature: "signature",
 	}
 }
--- go.mod
+++ go.mod
@@ -3,15 +3,23 @@ module github.com/yahoo/iframe-tests
 go 1.12
 
 require (
+	github.com/ardielle/ardielle-go v1.5.2
+	github.com/yahoo/athenz v1.8.23
+	github.com/yahoo/k8s-athenz-syncer v0.0.0-00010101000000-000000000000
 	k8s.io/api v0.0.0
+	k8s.io/apiextensions-apiserver v0.0.0-00010101000000-000000000000
 	k8s.io/apimachinery v0.0.0
 	k8s.io/apiserver v0.0.0
-	k8s.io/client-go v0.0.0
+	k8s.io/client-go v8.0.0+incompatible
+	k8s.io/component-base v0.0.0
 	k8s.io/klog v0.3.1
+	k8s.io/kube-aggregator v0.0.0-00010101000000-000000000000 // indirect
 	k8s.io/kubernetes v1.15.2
+	sigs.k8s.io/structured-merge-diff v0.0.0-20190724202554-0c1d754dd648 // indirect
 )
 
 replace (
+	github.com/yahoo/k8s-athenz-syncer => /Users/mcieplak/go/src/github.com/yahoo/k8s-athenz-syncer
 	k8s.io/api => k8s.io/api v0.0.0-20190805141119-fdd30b57c827
 	k8s.io/apiextensions-apiserver => k8s.io/apiextensions-apiserver v0.0.0-20190805143126-cdb999c96590
 	k8s.io/apimachinery => k8s.io/apimachinery v0.0.0-20190612205821-1799e75a0719
@@ -26,11 +34,13 @@ replace (
 	k8s.io/csi-translation-lib => k8s.io/csi-translation-lib v0.0.0-20190805144531-3985229e1802
 	k8s.io/kube-aggregator => k8s.io/kube-aggregator v0.0.0-20190805142416-fd821fbbb94e
 	k8s.io/kube-controller-manager => k8s.io/kube-controller-manager v0.0.0-20190805144128-269742da31dd
+	k8s.io/kube-openapi => k8s.io/kube-openapi v0.0.0-20190228160746-b3a7cee44a30
 	k8s.io/kube-proxy => k8s.io/kube-proxy v0.0.0-20190805143734-7f1675b90353
 	k8s.io/kube-scheduler => k8s.io/kube-scheduler v0.0.0-20190805144012-2a1ed1f3d8a4
 	k8s.io/kubelet => k8s.io/kubelet v0.0.0-20190805143852-517ff267f8d1
-	k8s.io/kubernetes => /work/gopath/src/k8s.io/kubernetes
+	k8s.io/kubernetes => /Users/mcieplak/go/src/k8s.io/kubernetes
 	k8s.io/legacy-cloud-providers => k8s.io/legacy-cloud-providers v0.0.0-20190805144654-3d5bf3a310c1
 	k8s.io/metrics => k8s.io/metrics v0.0.0-20190805143318-16b07057415d
 	k8s.io/sample-apiserver => k8s.io/sample-apiserver v0.0.0-20190805142637-3b65bc4bb24f
+	sigs.k8s.io/structured-merge-diff => sigs.k8s.io/structured-merge-diff v0.0.0-20190302045857-e85c7b244fd2
 )
